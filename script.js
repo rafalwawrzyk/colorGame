b95a448188d4290757bac175a179bb18a3431237
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2017-09-30 15:32:28 +0200

    add pick color functionality

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
         "rgb(255, 0, 255)",
     ]
     //variables
-    const pickedColor = colors[3];
+    const pickedColor = pickColor(colors);
     const squares = document.querySelectorAll('.square');
     const titleColor = document.querySelector('h1 span');
+    let message = document.querySelector('.winSpan');   
 
+    
+    
     titleColor.textContent = pickedColor;
 
 
@@ -23,14 +26,32 @@ document.addEventListener("DOMContentLoaded", function () {
         squares[i].style.backgroundColor = colors[i]
         //add events for all squares
         squares[i].addEventListener('click', function () {
-            if (this.style.backgroundColor === pickedColor) {
-                console.log("win")
+            //grab clicked color and compare to win color
+            const clickedColor = this.style.backgroundColor
+            if ( clickedColor === pickedColor) {
+                message.textContent = 'Correct'
+                correctColors(pickedColor);
             } else {
+                // add color of background to square
                 this.style.backgroundColor = "rgb(35, 35, 35)"
+                message.textContent = 'Try again'
             }
         })
     }
-
+    
+    
+    function correctColors(color){
+        //loop through all sqaures
+        squares.forEach(function(square){
+            // change all colors
+            square.style.backgroundColor = color;
+        })
+    }
+    
+    function pickColor(colors){
+        let randColor = colors[Math.floor(Math.random() * colors.length)]
+        return randColor
+        }
 
 
 
